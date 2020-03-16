1c04ba336753bccc2c34925331e53310c3af5f4e
MadCatMining <zuklys@mail.ru>
2020-03-16 15:25:02 +0200

    initial upload

---
+++ vanilla.h
@@ -0,0 +1,16 @@
+#ifndef VANILLA_H
+#define VANILLA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+void vanilla_hash(const char* input, char* output, uint32_t len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
